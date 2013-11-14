e2bb26310a464552f89b03e4d01a55ad6a9ac3c6
Mr. Lawrence <lawrence@server.fake>
2013-11-14 16:46:29 +0100

    format

--- src/erzeugerverbraucher/ErzeugerVerbraucherMain.java
+++ src/erzeugerverbraucher/ErzeugerVerbraucherMain.java
@@ -12,28 +12,27 @@ public class ErzeugerVerbraucherMain {
 		Integer limit = 8;
 		Integer erzeugerAmount = 5;
 		Integer verbraucherAmount = 5;
-		
-		LimitStack<HashMap<String, Integer>> sharedStack = new LimitStack<HashMap<String, Integer>>(limit);
+
+		LimitStack<HashMap<String, Integer>> sharedStack = new LimitStack<HashMap<String, Integer>>(
+				limit);
 		LOG.info("Stack mit Limit " + limit + " erzeugt");
 		ArrayList<Thread> erzeuger = new ArrayList<Thread>();
-		for(int i = 0; i<erzeugerAmount; i++) {
+		for (int i = 0; i < erzeugerAmount; i++) {
 			erzeuger.add(i, new Thread(new Erzeuger(sharedStack)));
 		}
-		
+
 		ArrayList<Thread> verbraucher = new ArrayList<Thread>();
-		for(int i = 0; i<verbraucherAmount; i++) {
+		for (int i = 0; i < verbraucherAmount; i++) {
 			verbraucher.add(i, new Thread(new Verbraucher(sharedStack)));
 		}
 
-		for(Thread t: erzeuger) {
-			t.start();
-		}
-		
-		for(Thread t: verbraucher) {
+		for (Thread t : erzeuger) {
 			t.start();
 		}
 
-		
+		for (Thread t : verbraucher) {
+			t.start();
+		}
 
 	}
 }
